7e202880f4dd6a9bac3942864b134dd355970aa6
Lorena <77021882+loresgarcia@users.noreply.github.com>
2022-10-04 23:30:32 -0300

    Menu Categorias adicionado

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,12 +1,12 @@
 import "./Cabecalho.css";
 import { Person, ShoppingCart } from "@mui/icons-material";
 
-export default function Cabecalho() {
+export default function Cabecalho({ setMenuCategoriasAtivo, menuCategoriasAtivo }) {
     return (
         <div className="cabecalho">
-            <img classname="cabecalho__logo" src="/imagens/kpop4u.svg" alt="logo kpop4u" />
+            <img className="cabecalho__logo" src="/imagens/kpop4u.svg" alt="logo kpop4u" />
             <p className="cabecalho__item">Início</p>
-            <p className="cabecalho__item">Categorias</p>
+            <p className="cabecalho__item" onClick={() => setMenuCategoriasAtivo(!menuCategoriasAtivo)}>Categorias</p>
             <p className="cabecalho__item">Sobre Nós</p>
             <p className="cabecalho__item">Contato</p>
             <Person className="cabecalho__perfil" />
