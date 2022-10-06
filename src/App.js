9c74c7576940955ba4cbfc417186b8953a3c2eb9
Lorena <77021882+loresgarcia@users.noreply.github.com>
2022-10-05 22:50:43 -0300

    Adicionando Login Page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Cabecalho from "./componentes/Cabecalho";
 import Lancamentos from "./componentes/Lancamentos";
 import MaisVendidos from "./componentes/MaisVendidos";
 import MenuCategorias from "./componentes/MenuCategoria";
+import Modal from "./componentes/Modal";
 import Rodape from "./componentes/Rodape";
 
 function App() {
   const [menuCategoriasAtivo, setMenuCategoriasAtivo] = useState(false);
+  const [loginAtivo, setLoginAtivo] = useState(false);
   
   return (
     <div>
-      <Cabecalho  menuCategoriasAtivo= {menuCategoriasAtivo} setMenuCategoriasAtivo={setMenuCategoriasAtivo}/>
+      <Cabecalho setLoginAtivo={setLoginAtivo} menuCategoriasAtivo= {menuCategoriasAtivo} setMenuCategoriasAtivo={setMenuCategoriasAtivo}/>
       <Lancamentos />
       <MaisVendidos />
       <Rodape />
       {menuCategoriasAtivo === true ?  <MenuCategorias /> : ""}
+      {loginAtivo === true ? <Modal setLoginAtivo={setLoginAtivo} /> : ""}
     </div>
   );
 }
--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,7 +1,7 @@
 import "./Cabecalho.css";
 import { Person, ShoppingCart } from "@mui/icons-material";
 
-export default function Cabecalho({ setMenuCategoriasAtivo, menuCategoriasAtivo }) {
+export default function Cabecalho({ setMenuCategoriasAtivo, menuCategoriasAtivo, setLoginAtivo }) {
     return (
         <div className="cabecalho">
             <img className="cabecalho__logo" src="/imagens/kpop4u.svg" alt="logo kpop4u" />
@@ -9,7 +9,8 @@ export default function Cabecalho({ setMenuCategoriasAtivo, menuCategoriasAtivo 
             <p className="cabecalho__item" onClick={() => setMenuCategoriasAtivo(!menuCategoriasAtivo)}>Categorias</p>
             <p className="cabecalho__item">Sobre Nós</p>
             <p className="cabecalho__item">Contato</p>
-            <Person className="cabecalho__perfil" />
+            <p className="cabecalho__visitante">Olá, Visitante!</p>
+            <Person className="cabecalho__perfil" onClick={() => setLoginAtivo(true)} />
             <ShoppingCart className="cabecalho__carrinho" />
         </div>
     )
